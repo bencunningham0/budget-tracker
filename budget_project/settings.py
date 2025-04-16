17a41ac9c62a16cb359bb1aa027e9af0a154154a
Ben Cunningham <191421776+bencunningham0@users.noreply.github.com>
2025-04-16 21:48:04 +0930

    Add 'django.contrib.humanize' to INSTALLED_APPS in settings.py

--- budget_project/settings.py
+++ budget_project/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     
     # Third party apps
     'social_django',
